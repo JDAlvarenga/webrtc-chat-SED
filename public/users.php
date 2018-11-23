c830244e607b4696c2b2133466ce2294d8ba3da0
Luis De Paz <lpaz@applaudostudios.com>
2018-11-23 01:07:20 -0600

    Added view for admins

---
+++ public/users.php
@@ -0,0 +1,38 @@
+</<!DOCTYPE html>
+<html>
+<head>
+    <title>WebChat P2P SED</title>
+  <link rel="stylesheet" href="src/css/main.css" type="text/css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="src/shared/semantic/semantic.min.css">
+  <link rel="stylesheet" href="src/css/webchat.css" type="text/css" media="screen" />
+</head>
+<body>
+<div class="ui one column doubling stackable grid container" id="loadingDesign">
+        <div class="column loginParts" id="whiteLoginPart" style="color:white;">
+            <h1>Listado de usuarios: <h1>
+            <?php
+                require 'vendor/autoload.php';
+                use Medoo\Medoo;
+                $database = new Medoo([
+                    // required
+                    'database_type' => 'pgsql',
+                    'database_name' => 'appchat',
+                    'server' => 'localhost',
+                    'username' => 'postgres',
+                    'password' => 'rootPass',
+                
+                    // [optional]
+                    'charset' => 'utf8mb4',
+                    'collation' => 'utf8mb4_general_ci',
+                    'port' => 5432
+
+                ]);
+                $results = $database->get("chat");
+                foreach ($results as $result) {
+                    echo "RESULT";
+                }
+            ?>
+        </div>
+</div>
+</body>
+</html>
