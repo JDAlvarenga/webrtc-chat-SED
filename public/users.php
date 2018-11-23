6beb9918716ffced60482ec53fba0e19951e6527
rgarcia7 <rgarcia7@zoho.eu>
2018-11-23 11:05:34 -0600

    Fix en users

--- public/users.php
+++ public/users.php
@@ -20,8 +20,8 @@ if ( !isset($_SESSION['user']) || $_SESSION['role'] == 2) {
 <html>
 <head>
     <title>WebChat P2P SED</title>
-  <link rel="stylesheet" href="src/css/main.css" type="text/css" media="screen" />
   <link rel="stylesheet" type="text/css" href="src/shared/semantic/semantic.min.css">
+  <link rel="stylesheet" href="src/css/main.css" type="text/css" media="screen" />
   <link rel="stylesheet" href="src/css/webchat.css" type="text/css" media="screen" />
 </head>
 <body>
@@ -53,25 +53,73 @@ if ( !isset($_SESSION['user']) || $_SESSION['role'] == 2) {
 
                 ]);
                 if(isset($_POST['userid'])){
-                    $updated_value = 1;
+                    if(isset($_POST['enable'])){
+                        $database->update("chat", [
+                           "active" => $updated_value
+                       ], ["id[=]" => $_POST['userid']]);
+                       $_POST['userid'] = null;
+                       $_POST['enable'] = null;
+                       $_POST['disable'] = null;
+                    }else{
+                     if(isset($_POST['disable'])){
+                        $database->update("chat", [
+                           "active" => $updated_value
+                       ], ["id[=]" => $_POST['userid']]);
+                       $_POST['userid'] = null;
+                       $_POST['enable'] = null;
+                       $_POST['disable'] = null;
+                    }   
+                    }
+
+                    /*$updated_value = 1;
                     if($_POST['userid'] == 1){
                         $updated_value = 0;
                     }
                    $database->update("chat", [
                        "active" => $updated_value
                    ], ["id[=]" => $_POST['userid']]);
+                   $_POST['userid'] = null;/*/
                 }
                 $results = $database->select("chat",["id","username", "active", "role"]);
                 echo "<p> Usuarios: </p><br/>";
-                echo ' <table style=\"color:white;\">
+                echo '
+                    <table class="ui collapsing celled table">
+                          <thead>
+                            <tr><th>Usuario</th>
+                            <th>Accion</th>
+                          </tr></tr></thead>
+                    <tbody>
+                    ';
+                /*echo ' <table style=\"color:white;\">
                         <tr>
                             <th>Username</th>
                             <th>Role</th>
                             <th>Status</th>
                             <th></th>
-                        </tr>';
+                        </tr>';*/
+
                 foreach ($results as $result) {
-                    echo "<tr>
+                    echo '
+                        <tr>
+                        <form method=\'post\' action=\'\'> 
+                          <td>
+                            <h4 class="ui image header">
+                              <div class="content">
+                                '.$result["username"].'
+                                <div class="sub header">'. $result["role"].'
+                              </div>
+                            </div>
+                          </h4></td>
+                          <td>
+                            <div class="ui left action input">
+                                '.get_button_message($result["active"]).'
+                                <input type=\'hidden\' name=\'userid\' id=\'userid\' value="'.$result["id"].'">
+                            </div>
+                          </td>
+                          </form>
+                        </tr>
+                        ';
+                    /*echo "<tr>
                             <form method='post' action=''> 
                             <td>".$result["username"]."</td>".
                             "<td>". $result["role"]."</td>".
@@ -80,19 +128,36 @@ if ( !isset($_SESSION['user']) || $_SESSION['role'] == 2) {
                                 <input type='hidden' name='userid' id='userid' value=\"".$result["id"]."\">
                             </td>
                             </form>
-                            </tr>";
+                            </tr>";*/
                 }
-
+                echo '
+                      </tbody>
+                </table>
+                ';
                 function get_button_message($string){
                     if($string == "1"){
-                        return "Desactivar";
+                        return '
+                                <button class="ui red icon button" type="submit">
+                                    Desactivar
+                                </button>
+                                <input type=\'hidden\' name=\'disable\' id=\'disable\' value="yes">
+                            ';
+                        //return "Desactivar";
                     }
                     else{
-                        return "Activar";
+                        return '
+                                <button class="ui green icon button" type="submit" >
+                                    Activar
+                                </button>
+                                <input type=\'hidden\' name=\'enable\' id=\'enable\' value="yes">
+                        ';
+                        //return "Activar";
                     }
                 }
             ?>
         </div>
 </div>
 </body>
+<script src="src/js/jquery-3.3.1.min.js"></script>
+<script src="src/shared/semantic/semantic.min.js"></script>
 </html>
